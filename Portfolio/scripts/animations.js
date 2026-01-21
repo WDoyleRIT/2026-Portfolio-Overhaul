dae1db6e6b1216ec292adc2c32dfb7ec320d1d06
Will Doyle <wbd6359@g.rit.edu>
2026-01-21 17:45:05 -0500

    CSS adjustments

--- Portfolio/scripts/animations.js
+++ Portfolio/scripts/animations.js
@@ -1,11 +1,34 @@
-window.onload = function(){
+const homeButton = document.getElementById("button1");
+const aboutButton = document.getElementById("button2");
+
+if (homeButton) {
+  homeButton.addEventListener("mouseenter", () => {
+    homeButton.classList.add("is-active");
+  });
+
+  homeButton.addEventListener("mouseleave", () => {
+    homeButton.classList.remove("is-active");
+  });
+}
+
+if (aboutButton) {
+  aboutButton.addEventListener("mouseenter", () => {
+    aboutButton.classList.add("is-active");
+  });
+
+  aboutButton.addEventListener("mouseleave", () => {
+    aboutButton.classList.remove("is-active");
+  });
+}
+
+/*window.onload = function(){
 setTimeout(() => {
       window.scrollTo({
         top: document.body.scrollHeight,
         behavior: "smooth",
       });
     }, 3000);
-}
+}*/ // Scrolls to the botton of the page after 3 seconds (Currently not in use)
 
 // Check if animations have already played this session
 if (sessionStorage.getItem("mainAnimated")) {
@@ -14,3 +37,4 @@ if (sessionStorage.getItem("mainAnimated")) {
   // Mark that animations have played
   sessionStorage.setItem("mainAnimated", "true");
 }
+
